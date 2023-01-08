d4c64c07d8a0ff22b055b8452901151a1bccd202
Alejandro Rivera <alexrivera78@gmail.com>
2023-01-07 20:02:42 -0600

    lab28 completed

---
+++ README.md
@@ -0,0 +1,35 @@
+# Lab: 28 - Django CRUDpy
+
+---
+
+### Project: Snacks CRUD
+### Author: Alejandro
+
+---
+
+Django project that adds full CRUD functionality to a "snacks" application. Allows user to Create, Read, Update and Delete "snacks"
+
+### Setup
+
+* Install dependencies in a `venv`
+  * **run:** pip install -r requirements.txt
+* Dependencies: 
+  * [requirements.txt](requirements.txt)
+
+### How to initialize/run your application
+
+* Initialize server:
+  * **run:** python3 manage.py runserver
+
+* Go to: [http://127.0.0.1:8000/](http://127.0.0.1:8000/) 
+
+### Links and Resources
+
+* app page: [snacks](http://127.0.0.1:8000/)
+* admin page: [snacks_admin](http://127.0.0.1:8000/admin)
+  * username: admin
+  * password: django
+
+### Tests
+
+* **run:** python3 manage.py test
--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,50 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from snacks.models import Snack
 
-# Create your tests here.
+
+class SnackTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='tester', email='tester@email.com', password='pass')
+        self.snack = Snack.objects.create(
+            title='snack', purchaser=self.user, description='description'
+        )
+
+    def test_snack_list(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_snack_detail(self):
+        url = reverse('snack_detail')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'snack-detail.html')
+
+    def test_snack_create(self):
+        url = reverse('snack_create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'snack-create.html')
+
+    def test_snack_update(self):
+        url = reverse('snack_update')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'snack-update.html')
+
+    def test_snack_delete(self):
+        url = reverse('snack_delete')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'snack-delete.html')
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.snack), 'snack')
+
+    def test_fields(self):
+        self.assertEqual(self.snack.title, 'snack')
+        self.assertEqual(self.snack.purchaser, self.user)
+        self.assertEqual(self.snack.description, 'description')
